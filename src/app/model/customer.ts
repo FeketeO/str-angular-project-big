5916c62cc3e02c2798dd234adad6a5749f5324c4
FeketeO <69634719+FeketeO@users.noreply.github.com>
2021-02-24 08:57:14 +0100

    customer list ts/html added

--- src/app/model/customer.ts
+++ src/app/model/customer.ts
@@ -2,8 +2,8 @@ import { Address } from "./address";
 
 export class Customer {
   id:number=0;
-  firstname:string='';
-  lastname:string='';
+  firstName:string='';
+  lastName:string='';
   address:Address= new Address;
   email:number=0;
   active:boolean=true;
