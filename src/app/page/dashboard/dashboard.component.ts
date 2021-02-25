99349782ca211fbb55b2a13f8bee0f42829b43aa
Fatrai1 <fatrai.cb@gmail.com>
2021-02-25 17:46:58 +0100

    add cards

--- src/app/page/dashboard/dashboard.component.ts
+++ src/app/page/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoCard } from 'src/app/common/info-card/info-card.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+
+  cards: InfoCard[] = [
+    {
+      title: 'Customers',
+      content: '102',
+      cardClass: 'card-header-warning',
+      footer: 'ide is jön valami',
+      icon: 'account_circle',
+    },
+    {
+      title: 'Products',
+      content: '50',
+      cardClass: 'card-header-success',
+      footer: 'ide is jön valami',
+      icon: 'store',
+    },
+    {
+      title: '3. card',
+      content: 'Hali',
+      cardClass: 'card-header-danger',
+      footer: 'ide is jön valami',
+      icon: 'info_outline',
+    },
+    {
+      title: '4. card',
+      content: 'Hali',
+      cardClass: 'card-header-info',
+      footer: 'ide is jön valami',
+      icon: 'update',
+    },
+  ]
 
   constructor() { }
 
