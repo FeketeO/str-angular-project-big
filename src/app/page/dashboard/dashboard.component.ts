21c8d861fab1d1ac0d9025f5e51f0850b8766764
Fatrai1 <fatrai.cb@gmail.com>
2021-03-02 21:50:49 +0100

    felső sor

--- src/app/page/dashboard/dashboard.component.ts
+++ src/app/page/dashboard/dashboard.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 import { InfoCard } from 'src/app/common/info-card/info-card.component';
 import { GraphCard } from 'src/app/common/graph-card/graph-card.component';
+import { BehaviorSubject, combineLatest, Observable, Subscription } from 'rxjs';
+import { Order } from 'src/app/model/order';
+import { ProductService } from 'src/app/service/product.service';
+import { OrderService } from 'src/app/service/order.service';
+import { CustomerService } from 'src/app/service/customer.service';
+import { BillService } from 'src/app/service/bill.service';
 
 
 
@@ -12,34 +18,37 @@ import { GraphCard } from 'src/app/common/graph-card/graph-card.component';
 })
 export class DashboardComponent implements OnInit {
 
+
+
+
   cards: InfoCard[] = [
     {
       title: 'Customers',
-      content: 0,
+      content: '0',
       cardClass: 'card-header-warning',
-      footer: 'ide is jön valami',
+      footer: 'Numbers of customers',
       icon: 'account_circle',
     },
     {
       title: 'Products',
-      content: 50,
+      content: '52',
       cardClass: 'card-header-success',
-      footer: 'ide is jön valami',
+      footer: 'Numbers os products',
       icon: 'store',
     },
     {
       title: 'Orders',
-      content: 50,
+      content: '30',
       cardClass: 'card-header-primary',
-      footer: 'ide is jön valami',
+      footer: 'Numbers of orders',
       icon: 'shopping_cart',
     },
     {
-      title: 'Followers',
-      content: 1,
+      title: 'Bill',
+      content: '12',
       cardClass: 'card-header-info',
-      footer: 'Follow us!',
-      icon: 'pan_tool',
+      footer: 'Numbers of bills',
+      icon: 'price_check',
     },
   ]
 
@@ -68,13 +77,34 @@ export class DashboardComponent implements OnInit {
 
   ]
 
-
+  combinatedSubscription: Subscription = new Subscription();
 
   constructor(
-
+    private productService:  ProductService,
+    private orderService: OrderService,
+    private customerService: CustomerService,
+    private billService: BillService,
   ) { }
 
   ngOnInit(): void {
+    this.combinatedSubscription = combineLatest([
+      this.productService.list$,
+      this.orderService.list$,
+      this.customerService.list$,
+      this.billService.billList$,
+    ]).subscribe(
+      data => {
+        this.cards[0].content = String(data[2].length);
+        this.cards[1].content = String(data[0].length);
+        this.cards[2].content = String(data[1].length);
+        this.cards[3].content = String(data[3].length);
+
+      }
+    );
+    this.productService.getAll();
+    this.orderService.getAll();
+    this.customerService.getAll();
+    this.billService.getAll();
   }
 
 }
